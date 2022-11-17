3c8b4575ef3330e3e6f3538c799b748dc1036cb5
Doug Barrett <doug.b.barrett@gmail.com>
2022-11-16 17:37:59 -0800

    updates to use proven in-memory cache

--- easycache.go
+++ easycache.go
@@ -1,110 +1,90 @@
 package easycache
 
 import (
+	"errors"
+	"fmt"
 	"sync"
 	"time"
+
+	"github.com/patrickmn/go-cache"
 )
 
 type Cache interface {
 	Get(key any) (any, error)
 }
 
-type l struct {
-	data any
-	_ttl time.Time
-	sync.RWMutex
-	done chan bool
-}
-
-type cache struct {
-	list   map[any]*l
+type service struct {
 	origin func(key any) any
-	_ttl   time.Duration
-	sync.RWMutex
-}
-
-func (c *cache) getTTL() time.Duration {
-	c.RLock()
-	defer c.RUnlock()
-	return c._ttl
+	cache  *cache.Cache
+	ttl    map[any]time.Time
+	keyTTL time.Duration
+	ttlRW  sync.RWMutex
 }
 
 func New(ttl time.Duration, fn func(key any) any) Cache {
-	var c = cache{
+	c := cache.New(ttl, ttl*2)
+
+	s := &service{
 		origin: fn,
-		_ttl:   ttl,
+		cache:  c,
 	}
 
-	c.list = make(map[any]*l)
+	s.ttl = make(map[any]time.Time)
+	s.keyTTL = ttl
 
-	go c.runner() // race condition panic
+	go s.runner() // race condition panic
 
-	return &c
+	return s
 }
 
-func (c *cache) runner() {
-
-	cacheTTL := c._ttl
-	ticker := time.NewTicker(cacheTTL)
+func (c *service) runner() {
+	ticker := time.NewTicker(c.keyTTL)
 	for range ticker.C {
-		count := len(c.list)
-		for i := 0; i < count; i++ { // rance condition panic
-			v, ok := c.list[i]
+		c.ttlRW.RLock()
+		ttlMap := make(map[any]time.Time)
+		for k, v := range c.ttl {
+			ttlMap[k] = v
+		}
+		c.ttlRW.RUnlock()
 
-			if !ok {
-				continue
-			}
-			v.RLock()
-			ttl := v._ttl
-			v.RUnlock()
-			if ttl.Before(time.Now().Add(-(5 * cacheTTL))) {
-				c.Lock()
-				delete(c.list, i)
-				c.Unlock()
+		for i := range ttlMap {
+
+			ttl := ttlMap[i]
+
+			if ttl.Add(c.keyTTL).Before(time.Now()) {
+				c.cache.Delete(fmt.Sprintf("%v", i))
 			}
 		}
 	}
 }
 
-func (c *cache) update(key any) {
-	chc, ok := c.list[key]
-	if !ok {
-		chc = &l{}
-		c.Lock()
-		c.list[key] = chc
-		c.Unlock()
-	}
-	chc.Lock()
-	chc.done = make(chan bool)
-	chc._ttl = time.Now().Add(c.getTTL())
-	chc.Unlock()
+func (c *service) update(key any) {
+	c.ttlRW.Lock()
+	c.ttl[key] = time.Now().Add(c.keyTTL)
+	c.ttlRW.Unlock()
 
-	chc.Lock()
-	defer chc.Unlock()
-	chc.data = c.origin(key)
-	chc._ttl = time.Now().Add(c.getTTL())
+	data := c.origin(key)
+	c.cache.Set(fmt.Sprintf("%v", key), data, cache.NoExpiration)
 }
 
-func (c *cache) Get(key any) (any, error) {
-	c.RLock()
-	val, ok := c.list[key]
-	c.RUnlock()
+func (c *service) Get(key any) (any, error) {
+	data, ok := c.cache.Get(fmt.Sprintf("%v", key))
 
 	if ok {
-		val.RLock()
-		vTTL := val._ttl
-		val.RUnlock()
+		c.ttlRW.Lock()
+		vTTL := c.ttl[key]
+		c.ttlRW.Unlock()
 		if time.Now().After(vTTL) {
 			go c.update(key)
 		}
-		val.RLock()
-		defer val.RUnlock()
-		return val.data, nil
+		return data, nil
 	}
 
 	c.update(key)
-	val, _ = c.list[key]
-	val.RLock()
-	defer val.RUnlock()
-	return val.data, nil
+
+	data, ok = c.cache.Get(fmt.Sprintf("%v", key))
+	if !ok {
+		return nil, errors.New("error fetching cache")
+	}
+	return data, nil
 }
